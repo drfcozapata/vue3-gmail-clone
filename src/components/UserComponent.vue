8b8315224a7271038c4f6d44049418516eb5a07d
Francisco Zapata <drfcozapata@gmail.com>
2022-12-26 16:10:00 -0400

    First commit

---
+++ src/components/UserComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="UserComponent" class="w-10">
+		<img
+			data-tooltip-target="tooltip-no-arrow-user"
+			data-tooltip-placement="bottom"
+			src="https://via.placeholder.com/40"
+			class="rounded-full w-10 cursor-pointer"
+		/>
+		<div
+			id="tooltip-no-arrow-user"
+			role="tooltip"
+			class="inline-block absolute invisible text-xs z-10 py-1 px-2 font-medium text-white rounded-sm shadow-sm opacity-0 tooltip dark:bg-gray-600 delay-150 bg-gray-800"
+		>
+			<div>Google Account</div>
+			<div class="text-gray-300">Francisco Zapata</div>
+			<div class="text-gray-300">drfcozapata@gmail.com</div>
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,79 @@
+<template>
+	<div id="EmailView" class="bg-gray-50 h-screen">
+		<div class="flex items-center p-2">
+			<div class="logo-section flex justify-start items-center px-3.5">
+				<IconComponent
+					icon-string="menu"
+					icon-color="#636363"
+					:icon-size="19"
+					hover-color="hover:bg-gray-200"
+					text="Main menu"
+					class="-ml-2 mr-2"
+				/>
+				<img width="107" src="/img/GmailLogo.png" alt="Gmail logo" />
+			</div>
+
+			<div class="flex w-full justify-between">
+				<div class="w-full bg-gray-200 flex items-center p-1 px-2 rounded-lg">
+					<IconComponent
+						icon-string="magnify"
+						icon-color="#636363"
+						:icon-size="19"
+						hover-color="hover:bg-gray-200"
+						text="Search"
+					/>
+					<input
+						class="w-full h-10 bg-gray-200 border-transparent border-none focus:ring-0 outline-none placeholder-gray-900"
+						type="text"
+						placeholder="Search email"
+					/>
+					<IconComponent
+						icon-string="tune"
+						icon-color="#636363"
+						:icon-size="19"
+						hover-color="hover:bg-gray-200"
+						text="Show search options"
+					/>
+				</div>
+				<div class="flex w-44 justify-between items-center ml-6 mr-2">
+					<IconComponent
+						icon-string="cog"
+						icon-color="#636363"
+						:icon-size="19"
+						hover-color="hover:bg-gray-200"
+						text="Settings"
+					/>
+					<IconComponent
+						icon-string="apps"
+						icon-color="#636363"
+						:icon-size="19"
+						hover-color="hover:bg-gray-200"
+						text="Google Apps"
+					/>
+
+					<UserComponent />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import IconComponent from '@/components/IconComponent.vue';
+	import UserComponent from '@/components/UserComponent.vue';
+</script>
+
+<style lang="scss" scoped>
+	#EmailView {
+		.logo-section {
+			min-width: 250px;
+		}
+	}
+	@media (max-width: 768px) {
+		#EmailView {
+			.logo-section {
+				min-width: 200px;
+			}
+		}
+	}
+</style>
